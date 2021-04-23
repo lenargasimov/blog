643e1086d9e1c84be24ea6e9764360ec417179d6
Lenar Gasimov <lv4262122+github@gmail.com>
2021-04-23 22:24:21 +0300

    feat: Add smtp.mail.yahoo.com

--- main.py
+++ main.py
@@ -182,7 +182,7 @@ def contact():
 
 def send_email(name, email, phone, message):
     email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
-    with smtplib.SMTP("smtp.gmail.com") as connection:
+    with smtplib.SMTP("smtp.mail.yahoo.com", 465) as connection:
         connection.starttls()
         connection.login(OWN_EMAIL, OWN_PASSWORD)
         connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
