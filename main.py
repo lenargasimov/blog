2f66d376570f1e693bb22c7401859fbec455b674
Lenar Gasimov <lv4262122+github@gmail.com>
2021-08-19 21:53:33 +0300

    style: added tab PEP 8

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import os
+import smtplib
+
+from datetime import datetime, date
+
 from flask import Flask, render_template, redirect, url_for, flash, abort, request
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
-from datetime import datetime, date
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
@@ -9,8 +13,7 @@ from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
 from flask_gravatar import Gravatar
-import smtplib
-import os
+
 
 OWN_EMAIL = os.environ.get("SECRET_EMAIL")
 OWN_PASSWORD = os.environ.get("SECRET_PASSWORD")
